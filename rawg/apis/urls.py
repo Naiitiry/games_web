c93416ea9de52fec49412c85474abaf9c6eb4475
Naiitiry <danchukroman@gmail.com>
2025-05-16 16:16:34 +0000

    filtrado de juegos por plataforma, dlc y orden

--- rawg/apis/urls.py
+++ rawg/apis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import landing_page_games
+from .views import landing_page_games,game_detail
 
 urlpatterns = [
     path('',landing_page_games,name="landing_page_games"),
+    path('game_detail/<int:game_id>',game_detail,name="game_detail"),
     
 ]
--- rawg/apis/views.py
+++ rawg/apis/views.py
@@ -9,21 +9,35 @@ API_KEY = settings.RAWG_API
 
 def landing_page_games(request):
     category = request.GET.get("ordering","metacritic")
+    platform_query = request.GET.get("platforms","")
     search_query = request.GET.get("search","")
+    exclude_additions = request.GET.get("exclude_additions", "")
     page = int(request.GET.get("page",1))
     next_page = page + 1
 
     base_url = "https://api.rawg.io/api/games"
     message_error =""
     games = []
+    platforms = []
+    additions = []
+
+    params={
+        "key":API_KEY,
+        "page":page,
+        "ordering":f'{category}'
+    }
 
 
     if search_query:
-        url = f'https://api.rawg.io/api/games?key={API_KEY}&search={search_query}&page={page}'
-    else:
-        url = f'{base_url}?key={API_KEY}&ordering={category}&page={page}'
+        params["search"] = search_query
+    if platform_query:
+        params["platforms"] = platform_query
+    if exclude_additions:
+        params["exclude_additions"] = exclude_additions
+    
+    # Try para los bucles de la lista de los juegos, con sus atributos
     try:
-        response = requests.get(url)
+        response = requests.get(base_url,params=params)
         data = response.json()
         count = data.get("count", 0)
         games = data.get("results",[])
@@ -31,29 +45,49 @@ def landing_page_games(request):
         total_pages = math.ceil(count/page_size) if page_size else 1
         has_next = page<total_pages
         # Ver por consola que devuelve la variable.
-        #print(games)
+        # print(games)
     except Exception as e:
         games = []
         message_error=f"Algo salió mal: {str(e)}"
         has_next = False
 
-    if request.headers.get("HX-Request"):
-        return render(request,"games/partials/_game_list.html",{"games":games,
-                                                "message_error":message_error,
-                                                "has_next":has_next,
-                                                "count":count,
-                                                "next_page":next_page,
-                                                "category":category
-                                                }) 
+    # Try para el bucle con las plataformas
+    try:
+        platforms_response = requests.get(f"https://api.rawg.io/api/platforms?key={API_KEY}")
+        platforms_response.raise_for_status()
+        platforms_data = platforms_response.json()
+        platforms = platforms_data.get("results", [])
+    except Exception as e:
+        platforms = []
+        message_error=f"Algo salió mal: {str(e)}"
+
+
+    context = {
+        "games": games,
+        "message_error": message_error,
+        "has_next": has_next,
+        "next_page": next_page,
+        "category": category,
+        "platform": platform_query,
+        "search_query": search_query,
+        "platforms":platforms,
+        "exclude_additions":exclude_additions
+    }
+    template = "games/partials/_game_list.html" if request.headers.get("HX-Request") else "games/landing.html"
     
-    return render(request,"games/landing.html",{"games":games,
-                                                "message_error":message_error,
-                                                "has_next":has_next,
-                                                "count":count,
-                                                "next_page":next_page,
-                                                "category":category
-                                                })
+    return render(request,template,context)
 
 def game_detail(request,game_id):
     game_detail_url = f'https://api.rawg.io/api/games/{game_id}?key={API_KEY}'
-    pass
+    error_message = ""
+
+    try:
+        game_detail_response = requests.get(game_detail_url)
+        game_detail_response.raise_for_status()
+        game_data = game_detail_response.json()
+    except Exception as e:
+        game_data = []
+        error_message = f"Error a obtener los datos del juego, {str(e)}"
+
+    return render(request,'games/games_detail.html',{"game":game_data,
+                                                     "error_message":error_message,})
